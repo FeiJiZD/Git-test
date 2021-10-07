ff21ee2f767fbbc82f4ff4ec073648b2677203e4
Songkj <feijzd@163.com>
2021-10-07 22:49:02 +0800

    Master Commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,13 @@
+
+
 /**
- * add -> commit
+ * 添加到暂存区->提交到本地库：add -> commit
  */
+//版本穿梭：左下角Git->log->右键 Checkout Revision
+
+//创建分支：右下角master->New Branch
+//切换分支：选中切换的分支->CheckOut
+//合并分支：选中合并的分支->merge into Current
 public class Test {
     public static void main(String[] args) {
         System.out.println("git-test");
